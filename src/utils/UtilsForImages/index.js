2f284a1c17661e61c382c615fa2e8e6bad98beee
lavlife06 <lav7990192369@gmail.com>
2022-05-09 15:31:40 +0530

    utils

---
+++ src/utils/UtilsForImages/index.js
@@ -0,0 +1,35 @@
+export const initialState = {
+  imagesList: [],
+  filteredImagesList: [],
+  paginationState: {
+    page: 1,
+    limit: 30,
+  },
+  loading: true,
+  singleImageDetailsLoading: true,
+  imagesListError: false,
+  singleImageDetailsError: false,
+  singleImageDetails: null,
+  currentImageIndex: -1,
+  searchString: '',
+};
+
+export const recursionTillGetToParentNode = (node) => {
+  if (node.id && node.id.includes('parentcard')) {
+    return node.id;
+  }
+  return recursionTillGetToParentNode(node.parentNode);
+};
+
+export const isMobileView = window.innerWidth <= 450 ? true : false;
+
+export const debounce = (func, delay) => {
+  let timer;
+  return function (...args) {
+    const context = this;
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(context, args);
+    }, delay);
+  };
+};
